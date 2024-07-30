c605a5c8c0dd48ff911901028397778497d2700f
JonatasNatanOFC <jonatas.22ti@gmail.com>
2024-07-30 16:12:00 -0300

    :tada: build: Criando app reactjs

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
+import { Container } from "react-bootstrap";
+
+function App() {
+
+  return (
+    <Container>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </Container>
+  )
+}
+
+export default App
